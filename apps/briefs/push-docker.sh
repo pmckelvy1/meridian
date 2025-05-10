2be1f29c8ef2ca432ab0ab341b7c5be15ecde5c7
Patrick McKelvy <pmckelvy1@gmail.com>
2025-05-10 12:38:54 -0400

    docker tweaks.

--- apps/briefs/push-docker.sh
+++
@@ -1,4 +0,0 @@
-# aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 605261817358.dkr.ecr.us-east-1.amazonaws.com
-# docker tag pmckelvy/meridian:latest 605261817358.dkr.ecr.us-east-1.amazonaws.com/pmckelvy/meridian:latest
-# docker push 605261817358.dkr.ecr.us-east-1.amazonaws.com/pmckelvy/meridian:latest
-
