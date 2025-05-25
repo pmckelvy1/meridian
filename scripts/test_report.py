b45c5b37109db5764095a4a90363623a564abec7
Patrick McKelvy <pmckelvy1@gmail.com>
2025-05-25 16:46:56 -0400

    iso time in test script.

--- scripts/test_report.py
+++ scripts/test_report.py
@@ -1,7 +1,10 @@
 import datetime
 import requests
 
-date = datetime.datetime.now()
+# current date without milliseconds
+# will use datetime.fromisoformat(date_str)
+date = datetime.datetime.now().isoformat()
 
 response = requests.get(f"http://ml.notawebsite.net:5000/api/generate-report?date={date}")
+print(response.status_code)
 print(response.json())
